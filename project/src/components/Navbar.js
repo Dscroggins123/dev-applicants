ef9439a10d299dfe3f6059b62f281242324a29a0
Dustin Scroggins <dscroggins123@gmail.com>
2022-02-07 23:26:42 -0800

    animated burger menu

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "../assets/Logo";
 import Burger from "../assets/Burger";
 
 const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false);
+
+  const animateMenu = () => {
+    setOpenNav(!openNav);
+    console.log(openNav);
+  };
+
   return (
-      <header className="navbar__container">
-        <div className="navbar__container-logo"><Logo/></div>
-    <nav>
-        <div className="navbar__container-burger" > <Burger/></div>
-      
-      <ul className="navbar__container-links">
-      <li>
-          <a href="#">Deposit</a>
-        </li>
-        <li>
-          <a href="#">Dashboard</a>
-        </li>
-        <li>
-          <a href="#">Company</a>
-        </li>
-        <li>
-          <a href="#">Pricing</a>
-        </li>
-      </ul>
-    </nav>
-      </header>
+    <header className="navbar__container">
+      <div className="navbar__container-logo">
+        <Logo />
+      </div>
+      <nav>
+        <div
+          className={`navbar__container-burger ${openNav ? "clicked" : ""}`}
+          onClick={animateMenu}
+        >
+          {" "}
+          <Burger />
+        </div>
+
+        <ul className={`navbar__container-links ${openNav ? "open" : ""}`}>
+          <li>
+            <a href="#">Deposit</a>
+          </li>
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Company</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
